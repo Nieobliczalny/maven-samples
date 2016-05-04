e8f10955a4bf039e712e57082c4d64c6bb1da6ee
Nieobliczalny <krychu1993@gmail.com>
2016-05-04 14:08:20 +0200

    Add test that should fail

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -30,4 +30,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void failedTest()
+  {
+    fail("It should fail!");
+  }
 }
